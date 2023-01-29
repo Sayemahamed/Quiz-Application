cef00523660aa093417783ddb8199a70d9d90544
Sayem <sayemahamed183@gmail.com>
2023-01-29 22:21:56 +0600

    Add project files.

---
+++ DataBase.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace QuizApp
+{
+    internal class DataBase
+    {
+        private SQLiteConnection connection = new SQLiteConnection("Data Source = Data.db");
+        private String ReadQuestion = "SELECT * FROM Question;";
+        private string ReadOptions = "SELECT * FROM Options;";
+        public void openConnection()
+        {
+            if (connection.State != System.Data.ConnectionState.Open) connection.Open();
+        }
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open) connection.Close();
+        }
+        public string[] Options(int ID)
+        {
+            string[] options = new string[4];
+            SQLiteCommand cmd = new SQLiteCommand(ReadOptions, connection);
+            SQLiteDataReader result = cmd.ExecuteReader();
+            while (result.Read())
+            {
+                if (Convert.ToInt32(result[0].ToString()) == ID)
+                {
+                    options[0] = result[1].ToString();
+                    options[1] = result[2].ToString();
+                    options[2] = result[3].ToString();
+                    options[3] = result[4].ToString();
+                    break;
+                }
+            }
+            return options;
+        }
+        public string[] Question(int ID)
+        {
+            string[] ans = new string[2];
+            SQLiteCommand cmd = new SQLiteCommand(ReadQuestion, connection);
+            SQLiteDataReader result = cmd.ExecuteReader();
+            while (result.Read())
+            {
+                if (Convert.ToInt32(result[0].ToString()) == ID)
+                {
+                    ans[0] = result[1].ToString();
+                    ans[1] = result[2].ToString(); break;
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace QuizApp
+{
+
+    public partial class MainWindow : Window
+    {
+        public int Score = 0, currentQS = 0;
+        private bool isChecked = true;
+        public char ans;
+        public string Question;
+        public string[] Options=new string[4];
+        private static  readonly DataBase dataBase = new DataBase();
+        private void firstTime()
+        {
+            dataBase.openConnection();
+            string[] temp = dataBase.Question(++currentQS);
+            Question = temp[0];
+            ans = temp[1].Trim()[0];
+            string[] tmp = dataBase.Options(currentQS);
+            Options[0] = tmp[0];
+            Options[1] = tmp[1];
+            Options[2] = tmp[2];
+            Options[3] = tmp[3];
+            dataBase.closeConnection();
+            QuestionBlock.Text = Question.Trim();
+            OptionA.Content = Options[0].Trim();
+            OptionB.Content = Options[1].Trim();
+            OptionC.Content = Options[2].Trim();
+            OptionD.Content = Options[3].Trim();
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            firstTime();
+        }
+        private void Next_QS(object sender, RoutedEventArgs e)
+        {
+            dataBase.openConnection();
+            string[] temp = dataBase.Question(++currentQS);
+            Question= temp[0];
+            ans = temp[1].Trim()[0];
+            string[] tmp = dataBase.Options(currentQS);
+            Options[0]= tmp[0];
+            Options[1]= tmp[1];
+            Options[2]= tmp[2];
+            Options[3]= tmp[3];
+            dataBase.closeConnection();
+            QuestionBlock.Text = Question.Trim();
+            OptionA.Content = Options[0].Trim();
+            OptionB.Content = Options[1].Trim();
+            OptionC.Content = Options[2].Trim();
+            OptionD.Content = Options[3].Trim();
+            OptionA.Background = Brushes.Transparent;
+            OptionB.Background = Brushes.Transparent;
+            OptionC.Background = Brushes.Transparent;
+            OptionD.Background = Brushes.Transparent;
+            isChecked = true;
+        }
+
+        private void A(object sender, RoutedEventArgs e)
+        {
+            updatePage('A');
+        }
+
+        private void B(object sender, RoutedEventArgs e)
+        {
+            updatePage('B');
+        }
+
+        private void C(object sender, RoutedEventArgs e)
+        {
+            updatePage('C');
+        }
+
+        private void D(object sender, RoutedEventArgs e)
+        {
+            updatePage('D');
+        }
+
+        public void updatePage(char selected)
+        {
+            if (selected == ans && isChecked) Score++;
+            isChecked = false;
+            ScoreText.Text = Score.ToString().Trim();
+            if (ans == 'A' || ans == 'a')
+                OptionA.Background = Brushes.Green;
+            if(ans=='B'||ans=='b')
+                OptionB.Background = Brushes.Green;
+            if (ans =='C'||ans=='c')
+                OptionC.Background = Brushes.Green;
+            if(ans=='D'||ans=='d')
+                OptionD.Background = Brushes.Green;
+        }
+    }
+}
